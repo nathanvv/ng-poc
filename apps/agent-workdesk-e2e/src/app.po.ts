37112f88f401bca5dbb0ae0528fea48a27f0f44e
Vaduganathan V Veerappan <25141@nelnet.net>
2018-06-17 20:10:38 -0500

    Base app

---
+++ apps/agent-workdesk-e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('suite-root h1')).getText();
+  }
+}
